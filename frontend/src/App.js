e9514931e23be17ef6ba433f1d63bbd45ba30c35
Collin DMello <cldmello@yahoo.com>
2021-11-28 21:53:24 -0500

    First API Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 function App() {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   };
+
+  // console.log(process.env);
 
   return (
     <div className="App">
