e001755e807e7c1026cbef85ae503f16947a3a3c
Collin DMello <cldmello@yahoo.com>
2021-12-30 21:09:55 -0500

    SVG Logo added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../img/logo.svg'; 
 
 const navbarStyle = {
-    backgroundColor: 'skyblue'
+    backgroundColor: '#eeeeee'
 };
 
 const Header = (props) => {
     return (
         <Navbar style={navbarStyle} variant="light">
             <Container>
-                <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+                <Logo style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
             </Container>
         </Navbar>
     )
