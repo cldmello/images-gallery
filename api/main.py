a391ed61d423b63710a0e7d995c0ae7288eefffd
Collin DMello <cldmello@yahoo.com>
2021-12-31 15:20:46 -0500

    API setup is active

---
+++ api/main.py
@@ -0,0 +1,38 @@
+import os
+import requests
+from flask import Flask, request
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+
+UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+UNSPLASH_KEY = os.environ.get('UNSPLASH_KEY', "")
+
+if not UNSPLASH_KEY:
+  raise EnvironmentError(".env file is missing or could not find environmnent variable UNSPLASH_KEY!")
+
+@app.route('/')
+def hello():
+  return "<h1>Hello World from Flask!</h1>"
+
+
+@app.route('/new-image')
+def new_image():
+  word = request.args.get('query')
+
+  cheaders = {
+    'Accept-Version': 'v1',
+    'Authorization': 'Client-ID ' + UNSPLASH_KEY
+  }
+  payload = {'query': word}
+
+  res = requests.get(url=UNSPLASH_URL, headers=cheaders, params=payload)
+  data = res.json()
+
+  return data
+
+# app.route('/')(hello) -- Above decorator expands to this line
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5050)
