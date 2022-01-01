5d0d1b6391e1116bacd55cc288629acacebd2d32
Collin DMello <cldmello@yahoo.com>
2021-12-31 23:55:38 -0500

    Created Dockerfiles for api and frontend

--- api/main.py
+++ api/main.py
@@ -8,33 +8,33 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)  # Cross-origin Resource Sharing enabled
 
-UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
-UNSPLASH_KEY = os.environ.get('UNSPLASH_KEY', "")
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 
 if not UNSPLASH_KEY:
-  raise EnvironmentError(".env file is missing or could not find environmnent variable UNSPLASH_KEY!")
+    raise EnvironmentError(
+        ".env file is missing or could not find environmnent variable UNSPLASH_KEY!"
+    )
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-  return "<h1>Hello World from Flask!</h1>"
+    return "<h1>Hello World from Flask!</h1>"
 
 
-@app.route('/new-image')
+@app.route("/new-image")
 def new_image():
-  word = request.args.get('query')
+    word = request.args.get("query")
 
-  cheaders = {
-    'Accept-Version': 'v1',
-    'Authorization': 'Client-ID ' + UNSPLASH_KEY
-  }
-  payload = {'query': word}
+    cheaders = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
+    payload = {"query": word}
+    res = requests.get(url=UNSPLASH_URL, headers=cheaders, params=payload)
+    data = res.json()
 
-  res = requests.get(url=UNSPLASH_URL, headers=cheaders, params=payload)
-  data = res.json()
+    return data
 
-  return data
 
 # app.route('/')(hello) -- Above decorator expands to this line
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5050)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050)
